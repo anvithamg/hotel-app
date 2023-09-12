bc927db6df0b312fcbb823e5c7eaefe32ab3461d
Gowda <gowdaan@apac.corpdir.net>
2023-09-12 19:35:56 +0530

    Hotel app

---
+++ src/app/reservation/reservation.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import { Reservation } from '../models/reservation';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ReservationService {
+  private reservations: Reservation[] = [];
+  constructor(){
+    let savedReservations = localStorage.getItem("reservations");
+    this.reservations = savedReservations ? JSON.parse(savedReservations):[];
+  }
+  //CRUD
+  getReservations() : Reservation[]{
+    return this.reservations;
+  }
+  getReservation(id:string) : Reservation | undefined{
+    return this.reservations.find(res=>res.id===id);
+  }
+  addReservation(newReservation:Reservation):void{
+    this.reservations.push(newReservation);
+    localStorage.setItem("reservations",JSON.stringify(this.reservations))
+   
+  }
+  deleteReservation(id:string){
+    let index = this.reservations.findIndex(res => res.id === id);
+    this.reservations.splice(index,1);
+    localStorage.setItem("reservations",JSON.stringify(this.reservations))
+  }
+  updateReservation(reservation:Reservation){
+    let index = this.reservations.findIndex(res => res.id == reservation.id)
+    this.reservations[index] = reservation;
+    localStorage.setItem("reservations",JSON.stringify(this.reservations))
+  }
+
+}
